a373e82efaa7a0ef1676b636c58d0aecf3b3ce39
Jorge <jormencar@yahoo.com>
2023-10-28 15:43:25 +0100

    adding auth ❌

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
 
 // InvoiceSchema
 const InvoiceSchema = z.object({
@@ -114,5 +115,17 @@ export async function deleteInvoice(formData: FormData) {
 		return { message: 'Deleted Invoice.' }
 	} catch (error) {
 		return { message: 'Database Error: Failed to Delete Invoice.' }
+	}
+}
+
+// Auth -------------------------------------------------------------------------
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+	try {
+		await signIn('credentials', Object.fromEntries(formData))
+	} catch (error) {
+		if ((error as Error).message.includes('CredentialsSignin')) {
+			return 'CredentialSignin'
+		}
+		throw error
 	}
 }
